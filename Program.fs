3aa7ad24f9391ea8ceb502b35809c43cf0b7efc5
Paul Kirby <kirby.paulh@gmail.com>
2022-12-07 22:41:27 -0500

    Code cleanup day 4

--- Program.fs
+++ Program.fs
@@ -181,14 +181,6 @@ let parseBadges (elves: string list): int =
 
 let cleaningData = File.ReadAllLines("cleanup.txt")
                 |> Array.toList
-let testData = [
-    "2-4,6-8";
-    "2-3,4-5";
-    "5-7,7-9";
-    "2-8,3-7";
-    "6-6,4-6";
-    "2-6,4-8";
-]
 
 let fullOverlap (pairs: int list): bool =
     let elf1: int * int = (pairs[0], pairs[1])
