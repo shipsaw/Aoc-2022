675e412de8e9a85a9bb0cdff682514228a2d80b6
Paul Kirby <kirby.paulh@gmail.com>
2022-12-08 01:48:24 -0500

    AOC Day 6 completed

--- Program.fs
+++ Program.fs
@@ -291,3 +291,24 @@ let runBulk (rawIns: string list) (initCrate: Crates): char list =
     |> List.map (fun lst -> lst.Head)
     
 // printf "Top crate on each stack: %s\n" (String.Concat(Array.ofList(runBulk craneInstructions crateList)))
+
+///////////////////// ADVENT DAY 6 ////////////////////////////
+
+//////////////// Part 1 ///////////////////////
+
+let dataStream = File.ReadAllText("dataStream.txt")
+
+let rec checkRepeat (len: int) (pos: int) (letters: char list) (msg: char list): int =
+    let lettersLen = min letters.Length (len - 1)
+    if letters.Length = len && (List.distinct letters).Length = len
+    then (pos - 1)
+    else checkRepeat len (pos + 1)  ((msg.Head :: letters)[..lettersLen]) msg.Tail
+    
+let runParse (len: int) (msg: string): int =
+    let listMsg = List.ofSeq msg
+    checkRepeat len 1 [] listMsg
+    
+printf $"Packet Position: %d{runParse 4 dataStream}\n"
+    
+//////////////// Part 1 ///////////////////////
+printf $"Header Position: %d{runParse 14 dataStream}\n"
